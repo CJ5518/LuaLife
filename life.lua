1ceb42d6504ca86bfdaa80bd814e1c9fa5a4cc0d
CarsonRueber <carsonrueber@gmail.com>
2024-03-06 12:47:32 -0800

    Move life to lua class

---
+++ life.lua
@@ -0,0 +1,61 @@
+--Holds a game of life
+
+local ffi = require("ffi");
+
+ffi.cdef[[
+void tickLifeOnBoard(void* boardRead, void* boardWrite, int width, int height)
+]];
+
+local lifeCLib = ffi.load("LuaLifeC")
+
+local life = {}
+life.__index = life
+
+
+function life.new(x,y)
+	if type(x) ~= "number" or type(y) ~= "number" then
+		error("Bad args to life.new, need 2 numbers width and height");
+	end
+
+	local o = {};
+	o.readCells = love.image.newImageData(x,y);
+	o.writeCells = love.image.newImageData(x,y);
+	o.image = love.graphics.newImage(o.readCells);
+	o.image:setFilter("nearest", "nearest");
+	setmetatable(o, life)
+
+	return o;
+end
+
+function life.getWidth(self)
+	return self.readCells:getWidth();
+end
+
+function life.getHeight(self)
+	return self.readCells:getHeight();
+end
+
+--Where on is 1 or 0
+function life.setPixel(self, x, y, on)
+	return self.readCells:setPixel(x,y,on,on,on,on);
+end
+
+function life.draw(self)
+	self.image:replacePixels(self.readCells)
+	love.graphics.draw(self.image,0,0,0,1,1);
+end
+
+function life.update(self)
+	lifeCLib.tickLifeOnBoard(
+		self.readCells:getFFIPointer(),
+		self.writeCells:getFFIPointer(),
+		self.readCells:getWidth(),
+		self.readCells:getHeight()
+	);
+
+	local tmp = self.readCells;
+	self.readCells = self.writeCells;
+	self.writeCells = tmp;
+end
+
+return life;
--- main.lua
+++ main.lua
@@ -1,39 +1,24 @@
-local ffi = require("ffi")
+local life = require("life");
 local readCells, writeCells;
 
-ffi.cdef[[
-void tickLifeOnBoard(void* boardRead, void* boardWrite, int width, int height)
-]]
 
-local testLib = ffi.load("LuaLifeC")
-
+local lifeInstance;
 
 function love.load()
-	
-	readCells = love.image.newImageData(love.graphics.getPixelDimensions());
-	for x=0, readCells:getWidth()-1 do
-		for y=0, readCells:getHeight()-1 do
-			if math.random() < .3 then
-				readCells:setPixel(x,y,1,1,1,1);
+	lifeInstance = life.new(love.graphics.getPixelDimensions());
+	for x=0, lifeInstance:getWidth()-1 do
+		for y=0, lifeInstance:getHeight()-1 do
+			if math.random() < .2 then
+				lifeInstance:setPixel(x,y,1);
 			end
 		end
 	end
-
-	
-	writeCells = love.image.newImageData(love.graphics.getPixelDimensions());
-	img = love.graphics.newImage(readCells)
-	img:setFilter("nearest", "nearest");
 end
 
 function love.draw()
-	img:replacePixels(readCells)
-	love.graphics.draw(img,0,0,0,1,1);
+	lifeInstance:draw();
 end
 
 function love.update()
-	testLib.tickLifeOnBoard(readCells:getFFIPointer(), writeCells:getFFIPointer(), readCells:getWidth(), readCells:getHeight());
-
-	local tmp = readCells;
-	readCells = writeCells;
-	writeCells = tmp;
+	lifeInstance:update();
 end
