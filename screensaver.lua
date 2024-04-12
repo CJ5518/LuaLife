20f37be4151c05066e2e36fe139cf4591997349f
CJ5518 <carsonrueber@gmail.com>
2024-04-12 12:25:12 -0700

    Fix screensaver mode

--- screensaver.lua
+++ screensaver.lua
@@ -1,28 +1,14 @@
 --Handles the actual screensaver portion of things
 --For the settings menu please see screensaverSettingsMenu.lua
 local life = require("life")
+local demoMode = require("demoMode");
 
 local screensaver = {}
 
 local lifeInstance;
 
 local function load()
-    lifeInstance = life.new(love.graphics.getPixelDimensions());
-    for x=0, lifeInstance:getWidth()-1 do
-        for y=0, lifeInstance:getHeight()-1 do
-            if math.random() < .2 then
-                lifeInstance:setPixel(x,y,1);
-            end
-        end
-    end
-end
-
-local function update(dt)
-    lifeInstance:update();
-end
-
-local function draw()
-	lifeInstance:draw();
+	demoMode.init()
 end
 
 local timesCloseProgramCalled = 0;
@@ -35,8 +21,6 @@ local function closeProgram()
 end
 
 function screensaver.init()
-	love.update = update;
-	love.draw = draw;
 	love.textinput = closeProgram;
 	love.keypressed = closeProgram;
 	love.keyreleased = closeProgram;
